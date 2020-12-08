20af17b4c71fb0e6861b1ad0e00303b2f0a805c5
no name <ginobilinie@users.noreply.github.com>
2020-12-08 00:40:28 -0800

    Delete __init__.py

--- furnace/datasets/cityscapes/__init__.py
+++
@@ -1,8 +0,0 @@
-from .ade import ADE
-from .voc import VOC
-from .cityscapes import Cityscapes
-from .deepglobe import Deepglobe
-from .roadextra import RoadExtra 
-from .putongroad import Putongroad
-
-__all__ = ['ADE', 'VOC', 'Cityscapes', 'Deepglobe', 'RoadExtra','Putongroad']
