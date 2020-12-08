1eb58d03442b72675f78a9643f6cb0f043554a8c
no name <ginobilinie@users.noreply.github.com>
2020-12-08 00:38:46 -0800

    Add files via upload

--- furnace/datasets/cityscapes/__init__.py
+++ furnace/datasets/cityscapes/__init__.py
@@ -1,1 +1,8 @@
+from .ade import ADE
+from .voc import VOC
+from .cityscapes import Cityscapes
+from .deepglobe import Deepglobe
+from .roadextra import RoadExtra 
+from .putongroad import Putongroad
 
+__all__ = ['ADE', 'VOC', 'Cityscapes', 'Deepglobe', 'RoadExtra','Putongroad']
