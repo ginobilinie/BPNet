bf961d62d00dc2777b0476f2e71f0f466282cfa7
no name <ginobilinie@users.noreply.github.com>
2020-12-08 01:04:09 -0800

    Update __init__.py

--- furnace/datasets/__init__.py
+++ furnace/datasets/__init__.py
@@ -1,8 +1,9 @@
 from .ade import ADE
 from .voc import VOC
 from .cityscapes import Cityscapes
-from .deepglobe import Deepglobe
-from .roadextra import RoadExtra 
-from .putongroad import Putongroad
+#from .deepglobe import Deepglobe
+#from .roadextra import RoadExtra 
+#from .putongroad import Putongroad
 
-__all__ = ['ADE', 'VOC', 'Cityscapes', 'Deepglobe', 'RoadExtra','Putongroad']
+__all__ = ['ADE', 'VOC', 'Cityscapes']
+#__all__ = ['ADE', 'VOC', 'Cityscapes', 'Deepglobe', 'RoadExtra','Putongroad']
